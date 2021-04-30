de327326f72aba11f88521465ae629c21aeaaa42
31Carlton7 <carltonaikins7@gmail.com>
2021-04-30 14:41:24 -0400

    Update smart_text_field.dart

--- lib/src/rich_text_editor/smart_text/smart_text_field.dart
+++ lib/src/rich_text_editor/smart_text/smart_text_field.dart
@@ -2,32 +2,41 @@ import 'package:canton_design_system/canton_design_system.dart';
 
 class SmartTextField extends StatelessWidget {
   const SmartTextField(
-      {Key key, this.type, this.controller, this.focusNode, this.autoFocus})
+      {Key key,
+      this.type,
+      this.controller,
+      this.focusNode,
+      this.autoFocus,
+      this.onChanged})
       : super(key: key);
 
   final SmartTextType type;
   final TextEditingController controller;
   final FocusNode focusNode;
   final bool autoFocus;
+  final void Function(String) onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      focusNode: focusNode,
-      autofocus: autoFocus ?? true,
-      keyboardType: TextInputType.multiline,
-      maxLines: null,
-      cursorColor: Theme.of(context).primaryColor,
-      textAlign: type.align,
-      decoration: InputDecoration(
-        border: InputBorder.none,
-        prefixText: type.prefix,
-        prefixStyle: type.textStyle,
-        isDense: true,
-        contentPadding: type.padding,
+    return Flexible(
+      child: TextField(
+        controller: controller,
+        focusNode: focusNode,
+        autofocus: autoFocus ?? true,
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+        cursorColor: Theme.of(context).primaryColor,
+        textAlign: type.align,
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixText: type.prefix,
+          prefixStyle: type.textStyle,
+          isDense: true,
+          contentPadding: type.padding,
+        ),
+        style: type.textStyle,
       ),
-      style: type.textStyle,
     );
   }
 }
