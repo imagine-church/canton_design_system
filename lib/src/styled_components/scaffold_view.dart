0a985a018c6826d7e7730b3976b98cff1f9390da
31Carlton7 <carltonaikins7@gmail.com>
2021-05-10 18:40:05 -0400

    Add BottomNavigationBar

--- lib/src/styled_components/scaffold_view.dart
+++ lib/src/styled_components/scaffold_view.dart
@@ -3,10 +3,12 @@ import 'package:canton_design_system/canton_design_system.dart';
 class CantonScaffold extends StatelessWidget {
   final Widget body;
   final EdgeInsets padding;
+  final BottomNavigationBar navBar;
 
   const CantonScaffold({
     Key key,
     this.body,
+    this.navBar,
     this.padding = const EdgeInsets.only(top: 17, left: 17, right: 17),
   }) : super(key: key);
   @override
@@ -15,6 +17,7 @@ class CantonScaffold extends StatelessWidget {
       onTap: () => CantonMethods.defocusTextfield(context),
       child: Scaffold(
         resizeToAvoidBottomInset: false,
+        bottomNavigationBar: navBar,
         body: SafeArea(
           child: Padding(
             padding: padding,
