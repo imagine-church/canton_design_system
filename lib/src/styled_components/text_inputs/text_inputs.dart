830b266ac3c71abf518fe8ba6bfb1b50bf4e7ae5
31Carlton7 <carltonaikins7@gmail.com>
2021-06-02 15:48:37 -0400

    Remove unnecessary elements from text_inputs.dart

--- lib/src/styled_components/text_inputs/text_inputs.dart
+++ lib/src/styled_components/text_inputs/text_inputs.dart
@@ -1,44 +1,26 @@
 import 'package:canton_design_system/canton_design_system.dart';
-import 'package:flutter/material.dart';
 
 class CantonTextInput extends StatelessWidget {
   final TextInputType textInputType;
   final String hintText, labelText;
   final Widget prefixIcon, suffixIcon;
   final FocusNode focusNode;
-  final bool obscureText, isTextFormField, isTextInputTwo, custom, autoFocus;
+  final bool obscureText, isTextFormField;
   final TextEditingController controller;
   final ValueChanged<String> onChanged;
-  final TextCapitalization textCapitalization;
-  final double containerWidth, containerHeight;
+  final double containerWidth;
   final int maxLines;
   final EdgeInsets containerPadding;
   final BorderRadius radius;
   final BorderSide border;
-  final Color containerColor,
-      enabledBorderColor,
-      errorBorderColor,
-      focusedBorderColor,
-      focusedErrorBorderColor,
-      cursorColor,
-      borderColor;
+  final Color containerColor;
 
   const CantonTextInput({
-    Key key,
     this.textInputType,
     this.containerColor,
     this.hintText,
     this.isTextFormField,
-    this.enabledBorderColor,
-    this.cursorColor,
-    this.errorBorderColor,
-    this.focusedBorderColor,
-    this.custom = false,
-    this.focusedErrorBorderColor,
-    this.borderColor,
     this.labelText,
-    this.isTextInputTwo,
-    this.autoFocus = false,
     this.prefixIcon,
     this.maxLines,
     this.border,
@@ -48,17 +30,15 @@ class CantonTextInput extends StatelessWidget {
     this.onChanged,
     this.obscureText,
     this.controller,
-    this.textCapitalization,
     this.containerWidth,
-    this.containerHeight,
     this.containerPadding,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    Widget labelTextWidget() {
+    Widget _labelTextWidget() {
       if (labelText != null) {
         return Column(
           children: [
@@ -74,7 +54,7 @@ class CantonTextInput extends StatelessWidget {
       }
     }
 
-    Widget prefixIconWidget() {
+    Widget _prefixIconWidget() {
       if (prefixIcon != null) {
         return Padding(
           padding: containerPadding ?? const EdgeInsets.all(19),
@@ -85,7 +65,7 @@ class CantonTextInput extends StatelessWidget {
       }
     }
 
-    Widget suffixIconWidget() {
+    Widget _suffixIconWidget() {
       if (suffixIcon != null) {
         return Padding(
           padding: containerPadding ?? const EdgeInsets.all(19),
@@ -96,219 +76,77 @@ class CantonTextInput extends StatelessWidget {
       }
     }
 
-    Widget textInputFieldOne() {
-      if (isTextFormField == true) {
-        return Material(
-          color: containerColor,
-          shape: SquircleBorder(
-            radius: 40,
-            side: border ??
-                BorderSide(
-                  color: borderColor ?? cantonGrey[400],
-                  width: 2,
-                ),
+    Widget _textInputFormField() {
+      return TextFormField(
+        obscureText: obscureText,
+        controller: controller,
+        onChanged: onChanged,
+        focusNode: focusNode,
+        maxLines: maxLines ?? 1,
+        cursorColor: Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+          /// To change height of Text Field, add contentPadding,
+          /// Use EdgeInsets.all,
+          /// and change it from 23 to something else.
+          /// 23 for height 65, 18 for height 50.
+          contentPadding: containerPadding ?? EdgeInsets.all(18),
+          hintText: hintText,
+          prefixIcon: _prefixIconWidget(),
+          suffixIcon: _suffixIconWidget(),
+          fillColor: containerColor,
+          enabledBorder: SquircleInputBorder(
+            radius: radius,
+            side: BorderSide(width: 1.5),
           ),
-          child: TextFormField(
-            obscureText: obscureText,
-            controller: controller,
-            autofocus: autoFocus,
-            maxLines: maxLines ?? 1,
-            decoration: InputDecoration(
-              /// To change height of Text Field, add contentPadding,
-              /// Use EdgeInsets.all,
-              /// and change it from 23 to something else.
-              /// 23 for height 65, 18 for height 50.
-              contentPadding: containerPadding ?? EdgeInsets.all(18),
-              hintText: hintText,
-              prefixIcon: prefixIconWidget(),
-              suffixIcon: suffixIconWidget(),
-            ),
+          focusedBorder: SquircleInputBorder(
+            radius: radius,
+            side: BorderSide(width: 1.5),
           ),
-        );
-      } else {
-        return Material(
-          color: containerColor,
-          shape: SquircleBorder(
-            radius: 40,
-            side: border ??
-                BorderSide(
-                  color: borderColor ?? cantonGrey[400],
-                  width: 2,
-                ),
-          ),
-          child: TextField(
-            obscureText: false,
-            onChanged: onChanged,
-            autofocus: autoFocus,
-            maxLines: maxLines ?? 1,
-            decoration: InputDecoration(
-              /// To change height of Text Field, add contentPadding,
-              /// Use EdgeInsets.all,
-              /// and change it from 23 to something else.
-              /// 23 for height 65, 18 for height 50.
-              contentPadding: containerPadding ?? EdgeInsets.all(18),
-              hintText: hintText,
-              prefixIcon: prefixIconWidget(),
-              suffixIcon: suffixIconWidget(),
-            ),
-          ),
-        );
-      }
+        ),
+      );
     }
 
-    Widget textInputFieldTwo() {
-      if (isTextFormField == true) {
-        return TextFormField(
-          obscureText: obscureText,
-          controller: controller,
-          onChanged: onChanged,
-          autofocus: autoFocus,
-          maxLines: maxLines ?? 1,
-          cursorColor: cursorColor ?? Theme.of(context).primaryColor,
-          decoration: InputDecoration(
-            /// To change height of Text Field, add contentPadding,
-            /// Use EdgeInsets.all,
-            /// and change it from 23 to something else.
-            /// 23 for height 65, 18 for height 50.
-            contentPadding: containerPadding ?? EdgeInsets.all(18),
-            hintText: hintText,
-            prefixIcon: prefixIconWidget(),
-            suffixIcon: suffixIconWidget(),
-            fillColor: containerColor ?? cantonGrey[200],
-            enabledBorder: custom
-                ? OutlineInputBorder(
-                    borderRadius: radius ?? BorderRadius.circular(17),
-                    borderSide: BorderSide(
-                      color: enabledBorderColor ?? CantonColors.transparent,
-                      width: 1.5,
-                    ),
-                  )
-                : SquircleInputBorder(
-                    radius: 45,
-                    side: BorderSide(
-                      color: enabledBorderColor ?? CantonColors.transparent,
-                      width: 1.5,
-                    ),
-                  ),
-            errorBorder: custom
-                ? OutlineInputBorder(
-                    borderRadius: radius ?? BorderRadius.circular(17),
-                    borderSide: BorderSide(
-                      color: errorBorderColor ?? cantonDanger,
-                      width: 1.5,
-                    ),
-                  )
-                : SquircleInputBorder(
-                    radius: 45,
-                    side: BorderSide(
-                      color: errorBorderColor ?? cantonDanger,
-                      width: 1.5,
-                    ),
-                  ),
-            focusedBorder: custom
-                ? OutlineInputBorder(
-                    borderRadius: radius ?? BorderRadius.circular(17),
-                    borderSide: BorderSide(
-                      color: focusedBorderColor ?? CantonColors.transparent,
-                      width: 1.5,
-                    ),
-                  )
-                : SquircleInputBorder(
-                    radius: 45,
-                    side: BorderSide(
-                      color: focusedBorderColor ?? CantonColors.transparent,
-                      width: 1.5,
-                    ),
-                  ),
-            focusedErrorBorder: custom
-                ? OutlineInputBorder(
-                    borderRadius: radius ?? BorderRadius.circular(17),
-                    borderSide: BorderSide(
-                      color: focusedErrorBorderColor ?? cantonDanger,
-                      width: 1.5,
-                    ),
-                  )
-                : SquircleInputBorder(
-                    radius: 45,
-                    side: BorderSide(
-                      color: focusedErrorBorderColor ?? cantonDanger,
-                      width: 1.5,
-                    ),
-                  ),
+    Widget _textInputField() {
+      return TextField(
+        obscureText: false,
+        onChanged: onChanged,
+        maxLines: maxLines ?? 1,
+        focusNode: focusNode,
+        cursorColor: Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+          /// To change height of Text Field, add contentPadding,
+          /// Use EdgeInsets.all,
+          /// and change it from 23 to something else.
+          /// 23 for height 65, 18 for height 50. WARNING: This can change if you have prefix Icon
+          fillColor: containerColor,
+          contentPadding: containerPadding ?? EdgeInsets.all(18),
+          hintText: hintText,
+          prefixIcon: _prefixIconWidget(),
+          suffixIcon: _suffixIconWidget(),
+          enabledBorder: SquircleInputBorder(
+            radius: radius,
+            side: BorderSide(width: 1.5),
           ),
-        );
-      } else {
-        return Material(
-          // shape: SquircleBorder(
-          //   radius: 40,
-          //   side: border ??
-          //       BorderSide(
-          //         color: borderColor ?? cantonGrey[400],
-          //         width: 2,
-          //       ),
-          // ),
-          child: TextField(
-            obscureText: false,
-            onChanged: onChanged,
-            maxLines: maxLines ?? 1,
-            autofocus: autoFocus,
-            cursorColor: cursorColor ?? Theme.of(context).primaryColor,
-            decoration: InputDecoration(
-              /// To change height of Text Field, add contentPadding,
-              /// Use EdgeInsets.all,
-              /// and change it from 23 to something else.
-              /// 23 for height 65, 18 for height 50. WARNING: This can change if you have prefix Icon
-              fillColor: containerColor ?? cantonGrey[200],
-              contentPadding: containerPadding ?? EdgeInsets.all(18),
-              hintText: hintText,
-              prefixIcon: prefixIconWidget(),
-              suffixIcon: suffixIconWidget(),
-              enabledBorder: SquircleInputBorder(
-                radius: 45,
-                side: BorderSide(
-                  color: enabledBorderColor ?? CantonColors.transparent,
-                  width: 1.5,
-                ),
-              ),
-              errorBorder: SquircleInputBorder(
-                radius: 45,
-                side: BorderSide(
-                  color: errorBorderColor ?? cantonDanger,
-                  width: 1.5,
-                ),
-              ),
-              focusedBorder: SquircleInputBorder(
-                radius: 45,
-                side: BorderSide(
-                  color: focusedBorderColor ?? CantonColors.transparent,
-                  width: 1.5,
-                ),
-              ),
-              focusedErrorBorder: SquircleInputBorder(
-                radius: 45,
-                side: BorderSide(
-                  color: focusedErrorBorderColor ?? cantonDanger,
-                  width: 1.5,
-                ),
-              ),
-            ),
+          focusedBorder: SquircleInputBorder(
+            radius: radius,
+            side: BorderSide(width: 1.5),
           ),
-        );
-      }
+        ),
+      );
     }
 
     Widget textInput() {
-      if (isTextInputTwo == true) {
+      if (isTextFormField) {
         return Padding(
           padding:
               EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: textInputFieldTwo(),
+          child: _textInputFormField(),
         );
       } else {
         return Padding(
           padding:
               EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: textInputFieldOne(),
+          child: _textInputField(),
         );
       }
     }
@@ -316,7 +154,7 @@ class CantonTextInput extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        labelTextWidget(),
+        _labelTextWidget(),
         Container(
           width: containerWidth ?? size.width,
           child: textInput(),
