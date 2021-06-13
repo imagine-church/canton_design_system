8d470fda3432ef5558e0f20253852baf88f75f8a
31Carlton7 <carltonaikins7@gmail.com>
2021-06-13 16:16:32 -0400

    Remove exports

--- lib/canton_design_system.dart
+++ lib/canton_design_system.dart
@@ -4,7 +4,6 @@ library canton_design_system;
 export 'package:circular_check_box/circular_check_box.dart';
 export 'package:emojis/emoji.dart';
 export 'package:emojis/emojis.dart';
-export 'package:cupertino_icons/cupertino_icons.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter_feather_icons/flutter_feather_icons.dart';
 export 'package:flutter_page_transition/flutter_page_transition.dart';
