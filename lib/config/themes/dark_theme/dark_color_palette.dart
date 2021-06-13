f6db4a184add471ee1b87ad2815990ff875f11a6
31Carlton7 <carltonaikins7@gmail.com>
2021-06-13 17:09:51 -0400

    Update dark_color_palette.dart

--- lib/config/themes/dark_theme/dark_color_palette.dart
+++ lib/config/themes/dark_theme/dark_color_palette.dart
@@ -196,6 +196,6 @@ class CantonDarkColors {
     onSecondary: gray[200],
     onBackground: black,
     onSurface: const Color(0xFF3c1e00),
-    onError: red[100],
+    onError: const Color(0xff4f0203),
   );
 }
